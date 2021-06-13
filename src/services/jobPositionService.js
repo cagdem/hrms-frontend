45f5aef59f960aa7635a4fce082d2de2c07181f8
Çağlar Demir <cagdem2@gmail.com>
2021-06-13 22:08:00 +0300

    post commands added to services

--- src/services/jobPositionService.js
+++ src/services/jobPositionService.js
@@ -4,4 +4,9 @@ export default class JobPositionService{
     getJobPositions(){
         return axios.get("http://localhost:8080/api/jobpositions/getall")
     }
+    postJobPositions(positionName){
+        return axios.post("http://localhost:8080/api/jobpositions/add",{
+            positionName: positionName
+        })
+    }
 }
